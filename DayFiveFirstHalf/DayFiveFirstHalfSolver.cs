bc9f0786127db4fa3792d1bd2aa3ee8cfd4a409d
Vinicius <vinitrech@gmail.com>
2025-12-27 10:56:32 -0300

    feat: improved performance and readability

--- DayFiveFirstHalf/DayFiveFirstHalfSolver.cs
+++ DayFiveFirstHalf/DayFiveFirstHalfSolver.cs
@@ -1,35 +1,20 @@
-﻿using System.Text.Json;
-
-namespace DayFiveFirstHalf;
+﻿namespace DayFiveFirstHalf;
 
 public static class DayFiveFirstHalfSolver
 {
     public static int SolveCafeteria(string[] input)
     {
-        var validIngredients = 0;
         var separator = input.IndexOf(string.Empty);
-        var ranges = input[..separator].Select(SetupRangeBoundaries);
-        var availableIngredients = input[(separator + 1)..].Select(ingredient => Convert.ToInt64(ingredient));
+        var ranges = input[..separator].Select(SetupRangeBoundaries).ToList();
+        var availableIngredients = input[(separator + 1)..].Select(long.Parse);
 
-        Console.WriteLine($"Got separator at index {separator}");
-        Console.WriteLine($"Got ranges: {JsonSerializer.Serialize(ranges.Select(range => string.Join("-", range.LeftBoundary, range.RightBoundary)))}");
-        Console.WriteLine($"Got available ingredients: {string.Join(",", availableIngredients.Select(i => i))}");
-
-        foreach (var availableIngredient in availableIngredients)
-        {
-            Console.WriteLine($"Evaluating ingredient: {availableIngredient}");
-
-            if (ranges.Any(range => range.LeftBoundary <= availableIngredient && range.RightBoundary >= availableIngredient))
-                validIngredients++;
-        }
-
-        return validIngredients;
+        return availableIngredients.Count(availableIngredient => ranges.Any(range => range.LeftBoundary <= availableIngredient && range.RightBoundary >= availableIngredient));
     }
 
     private static (long LeftBoundary, long RightBoundary) SetupRangeBoundaries(this string range)
     {
         var boundaries = range.Split('-');
 
-        return (LeftBoundary: Convert.ToInt64(boundaries[0]), RightBoundary: Convert.ToInt64(boundaries[1]));
+        return (LeftBoundary: long.Parse(boundaries[0]), RightBoundary: long.Parse(boundaries[1]));
     }
 }
