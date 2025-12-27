08fef81ee7d189879e8a44a74c22204a9bbc5b9a
Vinicius <vinitrech@gmail.com>
2025-12-27 10:47:25 -0300

    feat: solution for first half of day 5

---
+++ DayFiveFirstHalf/DayFiveFirstHalfSolver.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace DayFiveFirstHalf;
+
+public static class DayFiveFirstHalfSolver
+{
+    public static int SolveCafeteria(string[] input)
+    {
+        var validIngredients = 0;
+        var separator = input.IndexOf(string.Empty);
+        var ranges = input[..separator].Select(SetupRangeBoundaries);
+        var availableIngredients = input[(separator + 1)..].Select(ingredient => Convert.ToInt64(ingredient));
+
+        Console.WriteLine($"Got separator at index {separator}");
+        Console.WriteLine($"Got ranges: {JsonSerializer.Serialize(ranges.Select(range => string.Join("-", range.LeftBoundary, range.RightBoundary)))}");
+        Console.WriteLine($"Got available ingredients: {string.Join(",", availableIngredients.Select(i => i))}");
+
+        foreach (var availableIngredient in availableIngredients)
+        {
+            Console.WriteLine($"Evaluating ingredient: {availableIngredient}");
+
+            if (ranges.Any(range => range.LeftBoundary <= availableIngredient && range.RightBoundary >= availableIngredient))
+                validIngredients++;
+        }
+
+        return validIngredients;
+    }
+
+    private static (long LeftBoundary, long RightBoundary) SetupRangeBoundaries(this string range)
+    {
+        var boundaries = range.Split('-');
+
+        return (LeftBoundary: Convert.ToInt64(boundaries[0]), RightBoundary: Convert.ToInt64(boundaries[1]));
+    }
+}
