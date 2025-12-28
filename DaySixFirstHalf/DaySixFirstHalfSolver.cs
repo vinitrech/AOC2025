69b87c6c9cc031847c5ec1cfb6993832dd25953c
Vinicius <vinitrech@gmail.com>
2025-12-28 17:26:59 -0300

    feat: basic scratch logic for day 6 parte 1

--- DaySixFirstHalf/DaySixFirstHalfSolver.cs
+++ DaySixFirstHalf/DaySixFirstHalfSolver.cs
@@ -4,6 +4,25 @@ public static class DaySixFirstHalfSolver
 {
     public static long SolveTrashCompactor(string[] input)
     {
-        return 0L;
+        var grandTotal = 0L;
+        var entries = input.SelectMany(i => i.Split(string.Empty, options: StringSplitOptions.RemoveEmptyEntries)).ToList();
+        var (numbers, operators) = (entries[..^1].OrderByDescending(entry => entry.Length).ToList(), entries[^1]);
+        var index = 0;
+
+        Console.WriteLine($"Numbers:");
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.WriteLine($"Operators: {operators}");
+
+        // while(index < entries[0].Length)
+        // {
+        //     //TODO
+        // }
+
+        return grandTotal;
     }
 }
